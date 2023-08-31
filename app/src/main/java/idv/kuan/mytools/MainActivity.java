570b1d24165f2ecac03b8e36f129cbf44a16f475
willliu1981 <h111107231675>
2023-08-31 23:18:30 +0800

    init

---
+++ app/src/main/java/idv/kuan/mytools/MainActivity.java
@@ -0,0 +1,14 @@
+package idv.kuan.mytools;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
